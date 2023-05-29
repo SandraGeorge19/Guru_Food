06ef8ca51b0a5654e3c0db25c3b1699c381a7683
Sandra George <georgesandra592@gmail.com>
2023-05-29 21:59:21 +0300

    finish the logic of welcome screen

--- Guru Food/Modules/WelcomeScreen/WelcomeScreenViewController.swift
+++ Guru Food/Modules/WelcomeScreen/WelcomeScreenViewController.swift
@@ -9,15 +9,30 @@ import UIKit
 
 class WelcomeScreenViewController: UIViewController {
     // MARK: - Outlet(s)
-    @IBOutlet weak var welcomeView: WelcomeView!
+    @IBOutlet private weak var welcomeView: WelcomeView!
     // MARK: - LifeCycle(s)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let welcomeModel = WelcomeModel(title: "Food Recipies", subTitle: "Easy to make food")
-        welcomeView.setupUI(with: welcomeModel)
+        setupUI()
+        goToHomeAfterThreeSec()
     }
 
     // MARK: - Function(s)
+    func setupUI() {
+        let welcomeModel = WelcomeModel(title: "Food Recipies", subTitle: "Easy to make food")
+        welcomeView.setupUI(with: welcomeModel)
+    }
+    @objc
+    private func navigateToHome() {
+        let storyBoard = UIStoryboard(name: "Home", bundle: nil)
+        guard let homeVC = storyBoard.instantiateViewController(withIdentifier: String(describing: HomeViewController.self)) as? HomeViewController else { return }
+        homeVC.modalPresentationStyle = .fullScreen
+        self.present(homeVC, animated: false, completion: nil)
+    }
+    func goToHomeAfterThreeSec() {
+        let timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(navigateToHome), userInfo: nil, repeats: false)
+        // Add the timer to the main run loop to ensure it fires
+        RunLoop.main.add(timer, forMode: .common)
+    }
 }
-
